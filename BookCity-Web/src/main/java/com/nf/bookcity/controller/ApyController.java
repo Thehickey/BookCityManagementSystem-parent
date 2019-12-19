fec484f94f5857c3183a40274412b972d351b4cd
zjie <928952347@qq.com>
2019-12-20 07:33:20 +0800

    用户管理

--- BookCity-Web/src/main/java/com/nf/bookcity/controller/ApyController.java
+++ BookCity-Web/src/main/java/com/nf/bookcity/controller/ApyController.java
@@ -25,20 +25,6 @@ import java.util.*;
 
 @Controller
 public class ApyController {
-    private final String APP_ID = "2016101500691953";
-    private final String APP_PRIVATE_KEY = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCDYiAiRm9xRz+PEOB2yC18J+h+fDL0Oe3qEiJFEqT7Ccr/pjBzKujaRH/Vj4gI4KQ2yz2yeALu925OmXI+itdFLkfYCpCkeaoO5njAISIVKvunobnlmw3SXJ2XJqMyQI0vDaqlAUbBVgKVP8QlkMdI/0C5KJophSdqT91Fg1SKvzGjnnkpboCg6E4MqhHwJ1ppNjRtWiZvfnmfh4GqtXwlE0veE+DSu77ze06MNkPwho2U+v4bVqpQKHn9hDDXIlN9syzduT7XZiDKVg6ZJB105YTtOJtgJnx752jYGOpG58vaQVmiGbE4J3IpEA9pncU+ozO0fbAfvkrVzwVE/rvnAgMBAAECggEAYntaRPbxMENM5EutRbxVQ/ySLMWiWClYKh2/rHEGU9qKXMlRBfMDKiEwhX+aY7Unwp7kN3P4FvJHsAkbdsASDxxa5Bk/RNqh25tspe/wIVff3U7ADtkEKkwcIKHosJFsgvnGkPr8840qUH/E4re8ef5DoMdLj5Xs7qbA4QtJgtord0N5NE5ROd85EmWBuH7X+kOQn1diT5DJ4rOvVzx+wxO5g5wNj/+yw2ayba2JSOvVr8nr8RP1teeqHdr+EVWQDQvE0iUml6ocv0Ez/3lJJbqz1vt5rRnYjeciPZtUqUM+ieQH13oVdEFFRPxO3BHbuLQbhFXmg/ocsORzvoeJaQKBgQDNo0dVTNN14dVOWD7Qxa8pXbV9IgaseIwBVMGWhU+sehWWoRQ0Gp5C9VAixnnidKYPoYIpFgSPz8iu7g/PDPuJ0v6bDCR8s2nEPrSrdelBFjDobvQxLM7RaGjb1cOpmwQ48JisqxbEPK+75bd/lRPi0PwVGkQpc3pg6KElD4wyywKBgQCjj14AM+KexPiQD+SuFciHtjaqXaIKz2IbY7iqjxY5z9I9APp6bPk8i8fOBrg7nfEferBehf2pk1dLK3GBfNcTRulxbnmGqTN9FQzHPGdbIdhAvx5ypm3SprpWjwWbx+j5RTTKW80y2iy7BQGxJ2M+mzMV/cGiAvCZUdaBvYVL1QKBgEzGLRe3uytDyXOalrkzVRZImj3BumQiDYNhtXURCJIIco7uTLC3T2cIV4bkOxVFH8quLf5Q7hsTJIXK87QF0NPAJraUbfrwsQhHsQRi3VcjhdcBtcG2TPD53kxVHtvvuD/Z8+ZjafiR/7pTJZNIgUgpZjQqvlALgSE1ORSxHVLJAoGATqqo+o36BIUWTCvowDYn8eYY2s4YHvzp4eSPsDEL7phfNQogbvu00IMmgZhHyhzIGWzoIT5u572/MZ9eg3oo89Oaef05pCQDaLjUIYuS5okbuuBxY5FapwOcAf/eIbQAAMxhfKa0xi2bcAQTcUJRgI4i/wPCuwqQ8B0lf9dIolUCgYAQKAcHY0XTWqjQjuMDy8E5XTotmFHfk1RPrXrWLPiOF+lL+UOxjbvPCapoiHWitT8kkyEaGuSwU7KgyrBxarQ3+H3WiDOs7sVd6HZ3bmECaiGGaftb4XzNge6uVr37DVvcnvHE6xMxeqycR0hEfyfQzkpq0J/1B0S2po5C8lop7w==";
-    private final String CHARSET = "UTF-8";
-    //这个是支付宝公匙，不是应用公匙！！！
-    private final String ALIPAY_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlNvB7Gw1tyH47bB9983HITKX8fGwYh1ffPbN7S7OyW8lC0a7fQ6GwfnRfWTyL+6Wit21S1TPYWvnMqfNbNY49SRKmBq4L9UlWexAdVs4aa7gLNA02fgMlgaZkTL9cPpvejZ4p9+l5lGyvGh77SafbLnqcF6CgyFAgYgD8+HKsCGujFFZ1IwBBLwc5PlB2WpoW9RjATghTm7vdvpEUJuQiNS01eN9E174S9+ibNOLAXvuC+KJxSk18UJNoEhla7Ks4Pz3p7O1ED14nMQ9jv/ZJ9Ck6oRnk5yARHuhA90sVEX9BQVrnopz+zHqxByyhkcIi7hWqW3d6nIJj/xNGYR2FQIDAQAB";
-    //这是沙箱接口路径,正式路径为https://openapi.alipay.com/gateway.do
-    private final String GATEWAY_URL ="https://openapi.alipaydev.com/gateway.do";
-    private final String FORMAT = "JSON";
-    //签名方式
-    private final String SIGN_TYPE = "RSA2";
-    //支付宝异步通知路径,付款完毕后会异步调用本项目的方法,必须为公网地址
-    private final String NOTIFY_URL = "http://localhost:8080/notifyUrl";
-    //支付宝同步通知路径,也就是当付款完毕后跳转本项目的页面,可以不是公网地址
-    private final String RETURN_URL = "http://localhost:8080/returnUrl";
 
     @RequestMapping("/alipay")
     public void alipay(@RequestParam(value = "total",required = false)String total,
